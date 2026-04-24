171dec89f64470073235fa6e045e17cd3a138596
robiyann <robiyan@zyvenox.my.id>
2026-04-24 13:15:23 +0700

    Fix: update CSP with connect-src and allow Cloudflare Insights script

--- src/server.js
+++ src/server.js
@@ -57,7 +57,7 @@ app.use((req, res, next) => {
     res.setHeader('X-Frame-Options', 'SAMEORIGIN'); // Prevent being embedded in other sites
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com https:; img-src 'self' data: https:; frame-src 'self' data:;");
+    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com https://cdnjs.cloudflare.com https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com https:; img-src 'self' data: https:; connect-src 'self' https:; frame-src 'self' data:;");
     next();
 });
 
